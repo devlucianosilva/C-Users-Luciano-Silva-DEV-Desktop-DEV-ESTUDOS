2ef93482d67ac46409f38fc7ae046d96a1551db8
devlucianosilva <129123304+devlucianosilva@users.noreply.github.com>
2024-02-11 12:24:31 -0400

    estudo de classes em java

---
+++ EstudanteTest01.java
@@ -0,0 +1,15 @@
+public class EstudanteTest01{
+    public static void main (String[] args){
+        Estudante estudante = new Estudante();
+        estudante.nome = "leticia";
+        estudante.idade= 36;
+        estudante.sexo= 'F';
+
+
+
+        System.out.println("nome: "+estudante.nome);
+        System.out.println("nome: "+estudante.idade);
+        System.out.println("nome: "+estudante.sexo);
+        
+    }
+}
